9feaf1d6115734aeb14dc904d9ad8020e9fa9580
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-10-14 14:23:46 +0200

    Linking problems

--- opp.cc
+++ opp.cc
@@ -1,23 +1,14 @@
+#include <boost/regex.hpp>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <map>
-#include <version.hpp>
-#include "http_client.hpp"
 
-std::string getXml() {
-    HttpClient *client;
-    try {
-        boost::asio::io_service io_service;
-        client = new HttpClient(io_service, "www.currency-iso.org", "/dam/downloads/lists/list_one.xml");
-        io_service.run();
-    }
-    catch (std::exception &e) {
-        std::cout << "Exception: " << e.what() << "\n";
-    }
-    return client->getResponse();
+bool isISO4217valid(const std::string &code) {
+    boost::regex pattern("[A-Z]{3}");
+    return true;
 }
 
 int main() {
-    std::cout << getXml();
+
 }
